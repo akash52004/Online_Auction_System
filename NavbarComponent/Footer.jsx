3cb9c00d2129f6debcd248d287626fa73096c4d8
akash52004 <111482722+akash52004@users.noreply.github.com>
2024-08-03 11:14:58 +0530

    Add files via upload

---
+++ NavbarComponent/Footer.jsx
@@ -0,0 +1,164 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div>
+      <div class="container my-5">
+        <footer class="text-center text-lg-start text-color">
+          <div class="container-fluid p-4 pb-0">
+            <section class="">
+              <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color">
+                    Online Bidding System
+                  </h5>
+
+                  <p>
+                    Welcome to our online bidding hub, where sellers showcase,
+                    buyers bid, and thrilling auctions unfold. Explore, engage,
+                    and bid smartly for unparalleled experiences!
+                  </p>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">About us</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <p>
+                      <a href="#!" class="text-color">
+                      Our Mission
+                      Our mission is simple: to empower individuals and businesses to discover, bid on, and acquire a wide range of items with ease and confidence. We believe that every bid tells a story, and we're committed to creating a platform where those stories can unfold, whether it's finding that one-of-a-kind collectible, securing a great deal on everyday items, or connecting with fellow enthusiasts.
+                      
+                      </a>
+                    </p>
+                    <li>
+                      <a href="#!" class="text-color">
+                        
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">Contact us</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">Careers</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                  <h5 class="text-uppercase text-color-4">Links</h5>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 3
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#!" class="text-color">
+                        Link 4
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+
+            <hr class="mb-4" />
+
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3 text-color">Login from here</span>
+                <Link to="/user/login" class="active">
+                  <button
+                    type="button"
+                    class="btn btn-outline-light btn-rounded bg-color custom-bg-text"
+                  >
+                    Log in
+                  </button>
+                </Link>
+              </p>
+            </section>
+
+            <hr class="mb-4" />
+          </div>
+
+          <div class="text-center">
+            © 2023 Copyright:
+            
+            
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
